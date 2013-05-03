7ded668ab626e3643d9791b4d7c14c66ea5d2ac1
mikesol <msolisch@math.ups-tlse.fr>
2013-05-03 11:58:23 +0200

    Remove pkg and src files from git-cola

--- git-cola/pkg/usr/share/git-cola/lib/cola/widgets/defs.py
+++
@@ -1,4 +0,0 @@
-margin = 4
-spacing = 4
-handle_width = 4
-button_spacing = 12
--- git-cola/src/git-cola-1.8.2/extras/__init__.py
+++
@@ -1,8 +0,0 @@
-from extras.build_mo import build_mo
-from extras.build_pot import build_pot
-
-
-cmdclass = {
-    'build_mo': build_mo,
-    'build_pot': build_pot,
-}
